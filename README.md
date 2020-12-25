67b2cca18703f360370aa15d418355538f618202
Bryan Thomas <thomas.bryan.m@gmail.com>
2020-12-25 12:47:01 -0700

    update about and readme

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Inclusive Thesaurus
 
-### Easy Sarch to find alternatives to non-inclusive terms
+### Easy Search to find alternatives to non-inclusive terms
+
+As our language evolves so should our empathy, vocubulary, and word choice. The goal of this web app is to assist in the change of deeply embedded habits. We all can begin to consider the implications of words and phrases that have long gone unchallenged and unchanged. By using this search we can start to replace non-inclusiver terms with better alternatives. The list utilized by the app is by no means exhaustive but it gives a starting point.
+
+Thank you to the developers and contributors of the unified and retext packages. Please go check out the cool work going on over on those packages!
--- src/components/About/About.js
+++ src/components/About/About.js
@@ -10,8 +10,37 @@ export const About = () => {
     <Container>
       <Styles>
         <h2>About</h2>
-        <p>...Coming Soon</p>
-        <p></p>
+        <p>
+          As our language evolves so should our empathy, vocubulary, and word
+          choice. The goal of this web app is to assist in the change of deeply
+          embedded habits. We all can begin to consider the implications of
+          words and phrases that have long gone unchallenged and unchanged. By
+          using this search we can start to replace non-inclusiver terms with
+          better alternatives. The list utilized by the app is by no means
+          exhaustive but it gives a starting point.
+        </p>
+        <p>
+          Thank you to the developers and contributors of the unified and retext
+          packages. Please follow the link below to check out the cool work
+          going on over there!
+        </p>
+        <span>
+          <a
+            href="https://github.com/unifiedjs/unified"
+            rel="noreferrer"
+            target="_blank"
+          >
+            unified
+          </a>
+          <span> </span>
+          <a
+            href="https://github.com/retextjs/retext-equality"
+            rel="noreferrer"
+            target="_blank"
+          >
+            retext-equality
+          </a>
+        </span>
       </Styles>
     </Container>
   );
